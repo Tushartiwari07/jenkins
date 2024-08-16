7256bd069e6dabe64e5ef4d0fcd1d2184c79a320
vmahi <vmahi@Tushar>
2024-08-16 11:50:30 +0530

    OnPollSCM

---
+++ Jenkins/src/test/java/jenkins/OnPollScmTest.java
@@ -0,0 +1,13 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class OnPollScmTest {
+	@Test
+	public void onpoll()
+	{
+		Reporter.log("OnPollScmTest executed",true);
+	}
+
+}
